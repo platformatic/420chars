9529646d6633972f3802f56f50bec7fb92d1df67
Simon Plenderleith <simon@simonplend.co.uk>
2022-11-10 11:33:16 +0000

    [frontend] Add Astro React integration

--- apps/420chars-frontend/astro.config.mjs
+++ apps/420chars-frontend/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 })
